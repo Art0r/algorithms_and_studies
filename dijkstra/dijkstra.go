ef58388a6e743a2dc718f38afbf21a217d5f231f
Arthur Fernandes <arthur0139@gmail.com>
2025-09-19 15:28:06 -0300

    feat: complete dijkstra.go

--- dijkstra/dijkstra.go
+++ dijkstra/dijkstra.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"container/heap"
+	"fmt"
 	"math"
 )
 
-var graphs = [2]map[string]map[string]uint64{
+type GraphNodeToFind struct {
+	node_to_find string
+	graph        map[string]map[string]uint64
+}
+
+var graphs = []GraphNodeToFind{
 	{
-		"A": {"B": 5, "C": 2},
-		"B": {"E": 2, "D": 4},
-		"C": {"B": 8, "E": 7},
-		"D": {"F": 3, "E": 6},
-		"E": {"F": 1},
-		"F": {},
+		node_to_find: "F",
+		graph: map[string]map[string]uint64{
+			"A": {"B": 5, "C": 2},
+			"B": {"E": 2, "D": 4},
+			"C": {"B": 8, "E": 7},
+			"D": {"F": 3, "E": 6},
+			"E": {"F": 1},
+			"F": {},
+		},
 	},
 	{
-		"A": {"B": 10},
-		"B": {"D": 20},
-		"C": {"B": 1},
-		"D": {"E": 30, "C": 1},
-		"E": {},
+		node_to_find: "D",
+		graph: map[string]map[string]uint64{
+			"A": {"B": 10},
+			"B": {"D": 20},
+			"C": {"B": 1},
+			"D": {"E": 30, "C": 1},
+			"E": {},
+		},
 	},
 }
 
@@ -27,8 +39,8 @@ type Item struct {
 	// 'name' of the node in this case
 	value string
 	// weight of the node in this case
-	priority uint64
-	index    int
+	weight uint64
+	index  int
 }
 
 type PriorityQueue []*Item
@@ -50,7 +62,7 @@ func (pq PriorityQueue) Less(i, j int) bool {
 		return true
 	}
 
-	return pq[i].priority < pq[j].priority
+	return pq[i].weight < pq[j].weight
 }
 
 func (pq PriorityQueue) Swap(i, j int) {
@@ -76,7 +88,7 @@ func (pq *PriorityQueue) Pop() any {
 	return item
 }
 
-func dijkstra(graph map[string]map[string]uint64, start string) {
+func dijkstra(graph map[string]map[string]uint64, start string) (map[string]float64, map[string]any) {
 
 	/*
 	* estado inicial
@@ -107,17 +119,77 @@ func dijkstra(graph map[string]map[string]uint64, start string) {
 	var pq PriorityQueue = make(PriorityQueue, 0)
 	heap.Init(&pq)
 	heap.Push(&pq, Item{
-		value:    start,
-		priority: 0,
+		value:  start,
+		weight: 0,
 	})
 
 	for pq.Len() > 0 {
-		print(pq.Pop())
+		/*
+		* Removendo/Obtendo proximo node fazendo cast para Item
+		 */
+		node := pq.Pop().(*Item)
+
+		/*
+		* se a prioridade/custo do nó atual for maior
+		* que o contido na tabela de custos
+		* então passe para a proxima iteração
+		 */
+		if node.weight > uint64(costs[node.value]) {
+			continue
+		}
+
+		/*
+		* Investigando nós vizinhos para obter custos
+		* menores que o atual
+		 */
+		for node_value := range graph[node.value] {
+			node_weight := graph[node.value][node_value]
+			// um novo custo é calculado com o atual + o peso de seu vizinho
+			new_cost := node.weight + node_weight
+
+			/*
+			* Se achou um caminho mais eficiente para o vizinho
+			* considerando que o estado inicial é infinito então
+			* sempre será verdade, porém não necessariamente será
+			* verdade no caso de recalcular um valor que ja se tem
+			* conhecimento
+			 */
+			if new_cost < uint64(costs[node_value]) {
+
+				// atualiza o custo de deslocamento para o vizinho
+				costs[node_value] = float64(new_cost)
+
+				/*
+				* para que o caminho possa ser refeito então
+				* o vizinho mais eficiente é adicionado a lista
+				* de parents
+				 */
+				parents[node_value] = node_value
+
+				/*
+				 * adiciona o vizinho e seu custo para serem processados
+				 * na proxima iteração
+				 */
+				item := Item{
+					weight: new_cost,
+					value:  node_value,
+				}
+				pq = append(pq, &item)
+			}
+
+		}
+
 	}
+
+	return costs, parents
 }
 
 func main() {
 	for _, graph := range graphs {
-		dijkstra(graph, "A")
+		fmt.Println("===============================")
+		fmt.Println("Graph: ", graph.graph)
+		costs, _ := dijkstra(graph.graph, "A")
+		fmt.Println("Menor custo: ", costs[graph.node_to_find])
 	}
+	fmt.Println("===============================")
 }
